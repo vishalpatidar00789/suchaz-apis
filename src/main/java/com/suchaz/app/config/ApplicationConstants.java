c59915c24be98274fb7309431f12e5e425546d69
Anand <Anand@Asus>
2018-03-07 00:27:19 +0530

    Updating MetaData Functionality - [Anand]

--- src/main/java/com/suchaz/app/config/ApplicationConstants.java
+++ src/main/java/com/suchaz/app/config/ApplicationConstants.java
@@ -13,6 +13,34 @@ public final class ApplicationConstants {
     public static final String STORE = "store";
     public static final String CATEGORY = "category";
     public static final String FEATURED_PRODUCTS = "featuredProducts";
+    public static final String GENDER_TAG = "Gender";
+    public static final String AGE_GROUP_TAG = "AgeGroup";
+    
+    public static enum GENDER{
+    	MALE("Male"),FEMALE("Female");
+    	private String gender;
+    	GENDER(String gender)
+    	{
+    		this.gender = gender;
+    	}
+    public String getValue()
+    {
+    	return gender;
+    }
+    }
+    
+    public static enum AGE_GROUP{
+    	KIDS("Kids"),TEEN("Teen"),YOUTH("Youth"),ELDERS("Elders");
+    	private String ageGroup;
+    	AGE_GROUP(String ageGroup)
+    	{
+    		this.ageGroup = ageGroup;
+    	}
+    public String getValue()
+    {
+    	return ageGroup;
+    }
+    }
     
     private ApplicationConstants() {
     }
